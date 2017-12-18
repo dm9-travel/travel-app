b9a39d8548bbe9bf9502107359ca97c83f476b4d
Jack <jsjutzi@gmail.com>
2017-12-18 16:03:45 -0600

    axios call to externall api added

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -1,15 +1,25 @@
 module.exports = {
   Get_Flights: (req, res, next) => {
-    const dbInstance = req.app.get("db");
-    const {} = req.body;
-
-    dbInstance
-      .Get_Flights()
-      .then(flights => {
-        res.status(200).send(flights);
-      })
-      .catch(err => err);
+    const {
+      country,
+      currency,
+      locale,
+      originPlace,
+      destinationPlace,
+      outboundPartialDate,
+      inboundPartialDate,
+      key
+    } = req.body;
+    axios
+      .get(
+        `http://partners.api.skyscanner.net/apiservices/browsquotes/v1.0/${country}/${currency}/${locale}
+                /${originPlace}/${destinationPlace}/${outboundPartailDate}/${inboundPartialDate}?apiKey=${key}`
+      )
+      .then(response => {
+        return response.data;
+      });
   },
+
   Get_Watchlist: (req, res, next) => {
     const dbInstance = req.app.get("db");
     const { params } = req;
