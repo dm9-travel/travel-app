d295a814b158d39fb0caf1423d6efe373422347f
Dino Mandic <dinomandic@hotmail.com>
2017-12-18 23:06:22 -0600

    style: add logo to navBar and align links right

--- src/components/Nav/NavBar/NavBar.js
+++ src/components/Nav/NavBar/NavBar.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
 
+import logo from './logo.svg';
 import './NavBar.css';
 
 class NavBar extends Component {
-  constructor(props) {
-    super(props);
-
-  }
-
 
   render() {
 
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
-        <a className="navbar-brand" href="#">Travel App</a>
+        <img src={logo} className="logo" alt="logo"/>
+        <a className="navbar-brand" href="#">Wayz</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
+          <ul className="navbar-nav ml-auto">
             <li className="nav-item active">
               <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
             </li>
