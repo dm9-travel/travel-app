ee9ec0510132cd34c10c0f35870bdd82d16ab102
Jack <jsjutzi@gmail.com>
2017-12-19 10:45:11 -0600

    api data manipulation

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -12,10 +12,13 @@ module.exports = {
       inboundPartialDate,
       key
     } = req.body;
+    //http://partners.api.skyscanner.net/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=dallas&apiKey=so915963205287245322146323136295
+    //http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/US/USD/en-US/DFW/Anywhere/2018-03-03/?apiKey=so915963205287245322146323136295
     axios
       .get(
-        `http://partners.api.skyscanner.net/apiservices/browsquotes/v1.0/${country}/${currency}/${locale}
-                /${originPlace}/${destinationPlace}/${outboundPartailDate}/${inboundPartialDate}?apiKey=${key}`
+        `http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/${country}/${currency}/${locale}
+                /${originPlace}/${destinationPlace}/${outboundPartailDate}/${inboundPartialDate}?apiKey=${key}`,
+        { headers: { Accept: "application/json" } }
       )
       .then(response => {
         return response.data;
