ff637f4ad57b00252da23b2c2f117890a91b494b
ZachGerhart <zgerhart@gmail.com>
2018-01-02 14:10:20 -0600

    login and logout is rendering whether a user is logged in or not

--- src/components/Nav/NavBar/NavBar.js
+++ src/components/Nav/NavBar/NavBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux'
 import { requestUser } from '../../../ducks/user_reducer';
@@ -9,7 +10,9 @@ class NavBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      auth_id: null
+    };
 
     this.handleLogin = this.handleLogin.bind(this);
     this.handleLogout = this.handleLogout.bind(this);
@@ -25,25 +28,22 @@ class NavBar extends Component {
 
   componentDidMount() {
     this.props.requestUser()
+
+    axios.get('/api/me').then((response) => {
+    console.log(response.data[0].auth_id);
+    this.setState({auth_id: response.data[0].auth_id})
+        })
   }
 
 
   render() {
+   console.log(this.state.auth_id);
     let username = this.props.users.currentUser.user_name
     let navBarStyle = 'navbar sticky-top navbar-expand-lg navbar-light';
     let containerType = 'container';
-    let greeting = username ? <h1>Welcome {this.props.users.currentUser.user_name}!</h1> : null
+    let greeting = username ? <h5>Welcome {this.props.users.currentUser.user_name}!</h5> : null
     let renderAuth = !username
 
-      ?
-      <li className="nav-item">
-        <button className="btn btn-outline-primary log" onClick={this.handleLogin}>LOG IN</button>
-      </li>
-      :
-      <li className="nav-item">
-        <img src={this.props.picture} className="rounded-circle mr-4" height="40"></img>
-        <button className="btn btn-outline-danger log" onClick={this.handleLogout}>LOG OUT</button>
-      </li>
 
 
     if (this.props.location.pathname !== '/') {
@@ -72,6 +72,18 @@ class NavBar extends Component {
               </li>
 
                 {renderAuth}
+
+                {this.state.auth_id === null
+                ?
+                <li className="nav-item">
+                  <button className="btn btn-outline-primary log" onClick={this.handleLogin}>LOG IN</button>
+                </li>
+                :
+                <li className="nav-item">
+                  <img src={this.props.picture} className="rounded-circle mr-4" height="40"></img>
+                  <button className="btn btn-outline-danger log" onClick={this.handleLogout}>LOG OUT</button>
+                </li>
+              }
 
             </ul>
           </div>
