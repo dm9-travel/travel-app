c1dc0ae26f9bdf166c405aa310b6a8d5b22aa4cf
ZachGerhart <zgerhart@gmail.com>
2018-01-07 18:37:05 -0600

    trying to test

---
+++ src/components/Home/Home.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { shallow, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import renderer from "react-test-renderer";
+
+
+import Home from "./Home";
+
+test("Popular should render", () => {
+  configure(<Popular />);
+});
