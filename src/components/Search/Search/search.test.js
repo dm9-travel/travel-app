c0b77864beaeedeb122208dac870666055d30678
Jack <jsjutzi@gmail.com>
2018-01-11 12:27:46 -0600

    unit testing

--- src/components/Search/Search/search.test.js
+++ src/components/Search/Search/search.test.js
@@ -5,6 +5,9 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "../../../store";
 
+const flights = require("../../../ducks/flights_reducer.js");
+const search = require("./Search.js");
+
   
 describe('search', () => {
     it('renders without crashing', () => {
@@ -16,5 +19,29 @@ describe('search', () => {
                 </Provider>
             </BrowserRouter>, div);
     })
-});
+},
+describe('response should be an array', () => {
+    afterEach( () => {
+      search.handleSubmit = {
+          country: 'US',
+          currency: 'USD',
+          locale: 'en-US',
+          originPlace: 'DFW',
+          destinationPlace: 'Anywhere',
+          outboundPartialDate: '',
+          inboundPartialDate: '',
+          budget: 300
+        }
+    });
+    test('redux flight values should exist', () => {
+        expect(this.props.flights).not.toBe(null);
+    })
+})
+
+
+
+
+);
+
+
     
